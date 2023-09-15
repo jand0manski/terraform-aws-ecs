b939d721bddbf3d51bf2d42b68078967fecdb4fe
Jan Domanski <80462173+jand0manski@users.noreply.github.com>
2023-09-15 19:13:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "aws_service_discovery_http_namespace" "this" {
+  count = var.create_namespace?1:0 
+  name        = var.cluster_name
+  description = "CloudMap namespace for ${var.cluster_name}"
+}
+
 ################################################################################
 # Cluster
 ################################################################################
